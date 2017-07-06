882ee04e7da2cf2a7d4c3b7c09c41ae411918e79
terrabitz <trevor.taubitz@gmail.com>
2017-07-06 14:37:14 -0400

    Update run_mail loop until catching end signal

--- mongomail/run_mail.py
+++ mongomail/run_mail.py
@@ -1,4 +1,15 @@
+import signal
+import sys
 from mongomail.mail_app import controller
 
 if __name__ == '__main__':
     controller.start()
+    print('Mail server started.')
+    def signal_handler(signal, frame):
+        print('Ctrl+C pressed. Exiting...')
+        controller.stop()
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, signal_handler)
+    print('Press Ctrl+C')
+    signal.pause()
