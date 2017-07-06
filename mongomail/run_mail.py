6c5cd8db2217d7631ece63d4b0db506668f4abad
terrabitz <trevor.taubitz@gmail.com>
2017-07-06 16:06:51 -0400

    Updated run_mail to be run without __name__=="__main__"

--- mongomail/run_mail.py
+++ mongomail/run_mail.py
@@ -3,21 +3,22 @@ import sys
 import time
 from mongomail.mail_app import controller
 
-if __name__ == '__main__':
-    controller.start()
-    print('Mail server started.')
-    print('Press Ctrl+C to stop')
+controller.start()
+print('Mail server started.')
+print('Press Ctrl+C to stop')
 
-    def stop_service():
-        print('Ctrl+C pressed. Exiting...')
-        controller.stop()
-        sys.exit(0)
 
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        stop_service()
+def stop_service():
+    print('Ctrl+C pressed. Exiting...')
+    controller.stop()
+    sys.exit(0)
+
+
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    stop_service()
     # def signal_handler(signal, frame):
     #   stop_service()
     #
