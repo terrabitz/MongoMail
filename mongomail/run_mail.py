efc27173eb6983b59fe3bc17f208c302ac2112e7
terrabitz <trevor.taubitz@gmail.com>
2017-07-06 15:34:49 -0400

    Updated run_mail to use a looping signal catcher rather than a paused one

--- mongomail/run_mail.py
+++ mongomail/run_mail.py
@@ -1,15 +1,26 @@
 import signal
 import sys
+import time
 from mongomail.mail_app import controller
 
 if __name__ == '__main__':
     controller.start()
     print('Mail server started.')
-    def signal_handler(signal, frame):
+    print('Press Ctrl+C to stop')
+
+    def stop_service():
         print('Ctrl+C pressed. Exiting...')
         controller.stop()
         sys.exit(0)
 
-    signal.signal(signal.SIGINT, signal_handler)
-    print('Press Ctrl+C')
-    signal.pause()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        stop_service()
+    # def signal_handler(signal, frame):
+    #   stop_service()
+    #
+    # signal.signal(signal.SIGINT, signal_handler)
+    #
+    # signal.pause()
