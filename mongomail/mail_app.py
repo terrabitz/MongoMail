421a650df68c9571286f89e63dc47684bb43325c
terrabitz <trevor.taubitz@gmail.com>
2017-07-10 16:45:47 -0400

    Fixed mail_app config bug

--- mongomail/mail_app.py
+++ mongomail/mail_app.py
@@ -5,6 +5,6 @@ from mongoengine import connect
 from mongomail import config
 from mongomail.handler import MongoMailHandler
 
-connection = connect(config.MONGO_DB, host=config.MONGO_HOST, port=config.MONGO_PORT,
-                     password=config.MONGO_PASSWORD, username=config.MONGO_USER)
+connection = connect(config.MONGODB_DB, host=config.MONGODB_HOST, port=config.MONGODB_PORT,
+                     password=config.MONGODB_PASSWORD, username=config.MONGODB_USER)
 controller = Controller(MongoMailHandler(), hostname=config.MAIL_HOST, port=config.MAIL_PORT)
